62c5d69aeba03e2047d7c109a323b09c184d2d3e
farshazizi <farshazizi22@gmail.com>
2021-12-01 20:13:22 +0700

    Add csrf token and change title

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard - Mazer Admin Dashboard</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Dashboard - Bolu Ketan Cisadane</title>
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
