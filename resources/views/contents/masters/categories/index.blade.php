66783165a2369bc73181fb9fca0ad5b11936357e
farshazizi <farshazizi22@gmail.com>
2021-12-01 20:39:37 +0700

    Add view, add, edit and delete category module

---
+++ resources/views/contents/masters/categories/index.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.index')
+
+@section('content-header')
+    <h3>Kategori</h3>
+@endsection
+
+@section('content-body')
+    <section id="basic-horizontal-layouts">
+        <div class="row match-height">
+            <div class="col-md-12 col-12">
+                <div class="card">
+                    <div class="card-header ms-auto">
+                        <div class="buttons">
+                            <a href="{{ route('categories.create') }}" class="btn btn-primary">Tambah Kategori</a>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <table class="table" id="datatable">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Nama</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
+
+@section('content-js')
+    <script type="text/javascript">
+        var dataUrl = "{{ route('categories.data') }}";
+        var destroyUrl = "{{ route('categories.destroy', ':id') }}";
+    </script>
+    <script src="{{ asset(mix('js/contents/masters/categories/category.js')) }}"></script>
+@endsection
