c3b0b5457eb895bac0e2e6971120aa5e7c5eca12
farshazizi <farshazizi22@gmail.com>
2021-12-01 20:15:06 +0700

    Edit route and add class active

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -14,8 +14,8 @@
             <ul class="menu">
                 <li class="sidebar-title">Menu</li>
 
-                <li class="sidebar-item active ">
-                    <a href="index.html" class='sidebar-link'>
+                <li class="sidebar-item {{ (request()->is('/')) ? 'active' : '' }}">
+                    <a href="{{ route('/') }}" class='sidebar-link'>
                         <i class="bi bi-grid-fill"></i>
                         <span>Dashboard</span>
                     </a>
