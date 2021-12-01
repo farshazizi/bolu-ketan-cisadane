e92634fafa6e2b0f3354442fb2877e6ee6a070be
farshazizi <farshazizi22@gmail.com>
2021-12-01 20:35:48 +0700

    Delete unused code

--- resources/views/contents/masters/uoms/create.blade.php
+++ resources/views/contents/masters/uoms/create.blade.php
@@ -18,7 +18,7 @@
                         <div class="card-body">
                             @if (session('status'))
                                 <div class="alert alert-{{ session('status') }} alert-dismissible fade show" role="alert">
-                                    {{ session('message') }}.
+                                    {{ session('message') }}
                                     <button type="button" class="btn-close" data-bs-dismiss="alert"
                                         aria-label="Close"></button>
                                 </div>
