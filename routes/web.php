470d579d1883098d6a4d50190f571ebb05325b2f
farshazizi <farshazizi22@gmail.com>
2021-12-01 20:36:42 +0700

    Add route category module

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Masters\Categories\CategoryController;
 use App\Http\Controllers\Masters\Uoms\UomController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,6 +23,18 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
 
+// Category
+Route::prefix('/categories')->group(function () {
+    Route::get('/', [CategoryController::class, 'index'])->name('categories.index');
+    Route::get('/data', [CategoryController::class, 'data'])->name('categories.data');
+    Route::get('/create', [CategoryController::class, 'create'])->name('categories.create');
+    Route::post('/', [CategoryController::class, 'store'])->name('categories.store');
+    Route::get('/{id}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
+    Route::patch('/{id}', [CategoryController::class, 'update'])->name('categories.update');
+    Route::delete('/{id}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+});
+
+// Uom
 Route::prefix('/uoms')->group(function () {
     Route::get('/', [UomController::class, 'index'])->name('uoms.index');
     Route::get('/data', [UomController::class, 'data'])->name('uoms.data');
