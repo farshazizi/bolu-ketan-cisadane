673f4cb52ff1ad52860ec2acd553079780b0f024
farshazizi <farshazizi22@gmail.com>
2022-01-08 12:51:39 +0700

    Edit title tab application

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Dashboard - Bolu Ketan Cisadane</title>
+    <title>Bolu Ketan Cisadane</title>
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
