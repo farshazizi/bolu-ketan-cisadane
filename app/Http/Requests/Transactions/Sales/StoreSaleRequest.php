f3d8391082238c2e08090905a724256f260d083b
farshazizi <farshazizi22@gmail.com>
2022-02-05 20:47:55 +0700

    Add request sale module

---
+++ app/Http/Requests/Transactions/Sales/StoreSaleRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests\Transactions\Sales;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreSaleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'date' => 'required',
+            'notes' => 'nullable',
+            'grandTotal' => 'required',
+            'detail' => 'required|array',
+            'detail.*.inventoryStock' => 'required|string',
+            'detail.*.price' => 'required',
+            'detail.*.quantity' => 'required'
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'date.required' => 'Tanggal wajib diisi.',
+        ];
+    }
+}
