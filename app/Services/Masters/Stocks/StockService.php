bc7c23d85aeffa197f88a06d667d9baa33cccf44
farshazizi <farshazizi22@gmail.com>
2022-02-05 20:54:20 +0700

    Move call model to repository

--- app/Services/Masters/Stocks/StockService.php
+++ app/Services/Masters/Stocks/StockService.php
@@ -3,48 +3,33 @@
 namespace App\Services\Masters\Stocks;
 
 use App\Models\Masters\Stocks\Stock;
-use App\Models\Masters\Stocks\StockDetail;
+use App\Repositories\Masters\Stocks\StockRepository;
 use Exception;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
-use Ramsey\Uuid\Uuid;
 
 class StockService
 {
+    private $stockRepository;
+
+    public function __construct(StockRepository $stockRepository)
+    {
+        $this->stockRepository = $stockRepository;
+    }
+
     public function data()
     {
-        $data = Stock::all();
-        return $data;
+        $stocks = $this->stockRepository->getStocks();
+
+        return $stocks;
     }
 
     public function store($data)
     {
-        DB::beginTransaction();
         try {
-            $stock = new Stock;
-            $stockId = Uuid::uuid4();
-            $stock->id = $stockId;
-            $stock->date = $data['date'];
-            $stock->stock_type = $data['stockType'];
-            $stock->notes = $data['notes'];
-            $stock->save();
-
-            if ($data['inventoryStock']) {
-                for ($i = 0; $i < count($data['inventoryStock']); $i++) {
-                    $stockDetail = new StockDetail;
-                    $stockDetail->id = Uuid::uuid4();
-                    $stockDetail->stock_id = $stockId;
-                    $stockDetail->inventory_stock_id = $data['inventoryStock'][$i];
-                    $stockDetail->quantity = $data['quantity'][$i];
-                    $stockDetail->notes = $data['notesDetail'][$i];
-                    $stockDetail->save();
-                }
-            }
-            DB::commit();
+            $stock = $this->stockRepository->storeStock($data);
 
             return $stock;
         } catch (Exception $exception) {
-            DB::rollBack();
             Log::error($exception);
             throw new Exception('Stok masuk gagal ditambahkan.');
         }
@@ -53,26 +38,17 @@ class StockService
     public function getStockById($id)
     {
         $stock = Stock::has('stockDetails')->with('stockDetails.inventoryStock')->findOrFail($id);
+
         return $stock;
     }
 
     public function destroy($id)
     {
-        DB::beginTransaction();
         try {
-            $stockDetail = StockDetail::where('stock_id', $id);
-            $stockDetail->delete();
+            $stock = $this->stockRepository->destoryStockById($id);
 
-            $stock = Stock::findOrFail($id);
-            $stock->delete();
-            DB::commit();
-
-            return [
-                'stock' => $stock,
-                'stockDetail' => $stockDetail,
-            ];
+            return $stock;
         } catch (Exception $exception) {
-            DB::rollBack();
             Log::error($exception);
             throw new Exception('Stok gagal dihapus.');
         }
