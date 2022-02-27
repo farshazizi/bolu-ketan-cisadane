a09ddd8c099e92564e4ca45fd02294c7038bd33a
farshazizi <farshazizi22@gmail.com>
2022-02-27 23:14:20 +0700

    Add get stock function service

--- app/Services/Masters/Stocks/StockService.php
+++ app/Services/Masters/Stocks/StockService.php
@@ -4,15 +4,18 @@ namespace App\Services\Masters\Stocks;
 
 use App\Models\Masters\Stocks\Stock;
 use App\Repositories\Masters\Stocks\StockRepository;
+use App\Repositories\Transactions\Sales\SaleRepository;
 use Exception;
 use Illuminate\Support\Facades\Log;
 
 class StockService
 {
+    private $saleRepository;
     private $stockRepository;
 
-    public function __construct(StockRepository $stockRepository)
+    public function __construct(SaleRepository $saleRepository, StockRepository $stockRepository)
     {
+        $this->saleRepository = $saleRepository;
         $this->stockRepository = $stockRepository;
     }
 
@@ -51,6 +54,51 @@ class StockService
         } catch (Exception $exception) {
             Log::error($exception);
             throw new Exception('Stok gagal dihapus.');
+        }
+    }
+
+    public function getStockByInventoryStockId($id)
+    {
+        try {
+            $stockExist = 0;
+
+            // Get stock from stock in
+            $stockIn = $this->stockRepository->getStockInByInventoryStockId($id);
+
+            if ($stockIn) {
+                $stockFromStockIn = 0;
+                foreach ($stockIn as $item) {
+                    $stockFromStockIn += $item->quantity;
+                }
+            }
+
+            // Get stock from stock out
+            $stockOut = $this->stockRepository->getStockOutByInventoryStockId($id);
+
+            if ($stockOut) {
+                $stockFromStockOut = 0;
+                foreach ($stockOut as $item) {
+                    $stockFromStockOut += $item->quantity;
+                }
+            }
+
+            // Get stock from sale
+            $stockSale = $this->saleRepository->getStockByInventoryStockId($id);
+
+            if ($stockSale) {
+                $stockFromSale = 0;
+                foreach ($stockSale as $item) {
+                    $stockFromSale += $item->quantity;
+                }
+            }
+
+            // Calculate stock exist
+            $stockExist = $stockFromStockIn - $stockFromStockOut - $stockFromSale;
+
+            return $stockExist;
+        } catch (Exception $exception) {
+            Log::error($exception);
+            throw new Exception('Gagal mendapatkan stock.');
         }
     }
 }
