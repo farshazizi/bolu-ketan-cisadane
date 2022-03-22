9858ba24093d28d11250440e7514b48042a2bd3c
farshazizi <farshazizi22@gmail.com>
2022-03-22 23:17:22 +0700

    Add view pdf sale print out

---
+++ resources/views/contents/pdfs/sale.blade.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sale</title>
+</head>
+
+<body>
+    <div class="logo">
+        <table style="width: 100%">
+            <tbody>
+                <tr>
+                    <td style="text-align: center">
+                        <img src="data:image/png;base64, {{ $imageLogo }}" style="width: 200px" />
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <div class="header">
+        <table style="width: 100%; border-collapse: collapse">
+            <tbody>
+                <tr>
+                    <td>
+                        <h3
+                            style="background-color: #722C14; border-radius: 32px; color: white; padding: 8px; text-align: center">
+                            {{ $data->invoice_number }}
+                        </h3>
+                    </td>
+                    <td style="width: 40%"></td>
+                    <td>
+                        <h4>Karawang, {{ $data->date }} <br> Pukul {{ $data->time }}</h4>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <div class="body" style="margin-top: 48px">
+        <table style="width: 100%; border-collapse: collapse">
+            <thead style="background-color: #722C14; color: white">
+                <tr style="text-align: center">
+                    <td
+                        style="border-right: 1px white solid; border-top-left-radius: 8px; border-bottom-left-radius: 8px; padding: 8px">
+                        <b>No.</b>
+                    </td>
+                    <td style="border-right: 1px white solid"><b>NAMA BARANG</b></td>
+                    <td style="border-right: 1px white solid"><b>HARGA</b></td>
+                    <td style="border-right: 1px white solid"><b>JUMLAH BARANG</b></td>
+                    <td style="border-top-right-radius: 8px; border-bottom-right-radius: 8px"><b>TOTAL</b></td>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($data->saleDetails as $key => $item)
+                    <tr>
+                        <td
+                            style="border-right: 1px #722C14 solid; padding-top: 12px; padding-bottom: 12px; text-align: center">
+                            <p>
+                                {{ $key + 1 }}
+                            </p>
+                        </td>
+                        <td style="border-right: 1px #722C14 solid; padding-top: 12px; padding-bottom: 12px">
+                            <p style="margin: 8px">
+                                {{ $item->inventoryStock->name }}
+                            </p>
+                        </td>
+                        <td
+                            style="border-right: 1px #722C14 solid; padding-top: 12px; padding-bottom: 12px; text-align: right">
+                            <p style="margin: 8px">
+                                {{ number_format($item->price, 0) }}
+                            </p>
+                        </td>
+                        <td
+                            style="border-right: 1px #722C14 solid; padding-top: 12px; padding-bottom: 12px; text-align: center">
+                            <p>
+                                {{ $item->quantity }}
+                            </p>
+                        </td>
+                        <td style="padding-top: 12px; padding-bottom: 12px; text-align: right">
+                            <p style="margin: 8px">
+                                {{ number_format($item->total, 0) }}
+                            </p>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+    <div class="footer" style="margin-top: 16px">
+        <table style="width: 100%">
+            <tbody>
+                <tr>
+                    <td style="text-align: right">
+                        <p style="font-size: 24px">
+                            <b>Total Rp{{ number_format($data->grand_total, 0) }}</b>
+                        </p>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+
+</html>
