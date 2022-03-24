cd589bbe0deeb13bf8529a19ccc24244327931ac
farshazizi <farshazizi22@gmail.com>
2022-03-24 11:37:55 +0700

    Add font family

--- resources/views/contents/pdfs/sale.blade.php
+++ resources/views/contents/pdfs/sale.blade.php
@@ -6,6 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Penjualan {{ $data->invoice_number }}</title>
+    <style>
+        html,
+        body {
+            font-family: 'Myriad', sans-serif;
+            font-weight: normal;
+        }
+
+    </style>
 </head>
 
 <body>
