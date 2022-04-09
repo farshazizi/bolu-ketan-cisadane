2c8ee3446a3c8366cb1437211cefad8110db1fa4
farshazizi <farshazizi22@gmail.com>
2022-04-10 01:44:33 +0700

    Delete unused code

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,11 +2,6 @@
     <div class="sidebar-wrapper active">
         <div class="sidebar-header">
             <div class="d-flex justify-content-between">
-                {{-- <div class="logo">
-                    <a href="{{ route('/') }}">
-                        <img src="{{ asset('assets/images/logo/logo.png') }}" alt="Logo">
-                    </a>
-                </div> --}}
                 <div class="toggler">
                     <a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
                 </div>
