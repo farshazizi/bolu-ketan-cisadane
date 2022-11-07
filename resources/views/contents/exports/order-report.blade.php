221a754309432d75e2eeb1bc0ef04a8da6593afd
farshazizi <farshazizi22@gmail.com>
2022-11-07 16:19:13 +0700

    Edit view order report

--- resources/views/contents/exports/order-report.blade.php
+++ resources/views/contents/exports/order-report.blade.php
@@ -1,47 +1,36 @@
 <table>
     <thead>
         <tr>
-            <th style="width: 100px"><b>LAPORAN PESANAN HARIAN</b></th>
+            <th><b>LAPORAN PENJUALAN HARIAN</b></th>
+        </tr>
+        <tr>
+            <th rowspan="2" style="width: 150px; text-align: center"><b>No</b></th>
+            <th rowspan="2" style="width: 150px; text-align: center"><b>Transaksi</b></th>
+            <th colspan="{{ count($inventoryStocks) }}" style="width: 150px; text-align: center"><b>Stock</b></th>
+        </tr>
+        <tr>
+            @foreach ($inventoryStocks as $inventoryStock)
+                <th style="width: 150px; text-align: center">{{ $inventoryStock['name'] }}</th>
+            @endforeach
         </tr>
     </thead>
     <tbody>
-        @foreach ($data as $item)
+        @foreach ($orders as $key => $order)
             <tr>
-                <td style="width: 100px"><b>Tanggal</b></td>
-                <td>{{ $item->date }}</td>
+                <td style="text-align: center">{{ $key + 1 }}</td>
+                <td style="text-align: right">{{ $order['name'] }}</td>
+                @foreach ($order['orderDetails'] as $orderDetail)
+                    <td style="text-align: right">{{ $orderDetail['quantity'] }}</td>
+                @endforeach
             </tr>
-            <tr>
-                <td style="width: 100px"><b>Nama</b></td>
-                <td>{{ $item->name }}</td>
-            </tr>
-            <tr>
-                <td style="width: 100px"><b>Alamat</b></td>
-                <td>{{ $item->address }}</td>
-            </tr>
-            <tr>
-                <td style="width: 100px"><b>No. Telepon</b></td>
-                <td>{{ $item->phone }}</td>
-            </tr>
-            <tr>
-                <td style="width: 100px"><b>Notes</b></td>
-                <td>{{ $item->notes }}</td>
-            </tr>
-            <tr>
-                <td colspan="3" style="background-color: $CEE2F2; text-align: center"><b>Pesanan</b></td>
-            </tr>
-            <tr>
-                <td style="width: 100px; text-align: center"><b>No</b></td>
-                <td style="width: 150px"><b>Nama Item</b></td>
-                <td style="width: 100px; text-align: center"><b>Jumlah Item</b></td>
-            </tr>
-            @foreach ($item->orderDetails as $key => $itemOrderDetail)
-                <tr>
-                    <td style="width: 100px; text-align: center">{{ $key + 1 }}</td>
-                    <td style="width: 150px">{{ $itemOrderDetail->inventoryStock['name'] }}</td>
-                    <td style="width: 100px; text-align: center">{{ $itemOrderDetail['quantity'] }}</td>
-                </tr>
-            @endforeach
-            <tr></tr>
         @endforeach
     </tbody>
+    <tfoot>
+        <tr>
+            <td colspan="2" style="text-align: center"><b>Jumlah</b></td>
+            @foreach ($totalOrders as $totalOrder)
+                <td>{{ $totalOrder['quantity'] }}</td>
+            @endforeach
+        </tr>
+    </tfoot>
 </table>
